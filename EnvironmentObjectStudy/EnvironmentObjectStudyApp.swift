f9dec5dead25194013d72adda588007d39bcb2ec
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-29 12:04:04 +0300

    Initial Commit

---
+++ EnvironmentObjectStudy/EnvironmentObjectStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectStudyApp.swift
+//  EnvironmentObjectStudy
+//
+//  Created by Ömer Faruk Kılıçaslan on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
